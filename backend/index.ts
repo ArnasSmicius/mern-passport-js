f7180fb12c84ec7b9f519855c37a7e6e42cf1e81
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2023-07-17 16:48:38 +0300

    Connect to MongoDB

--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,44 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors, { CorsOptions } from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  // Load environment variables from .env file in non prod environments
+  dotenv.config();
+}
+require("./utils/connectdb");
 
 const app: Express = express();
-const port = process.env.PORT ? process.env.PORT : 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//Add the client URL to the CORS policy
+const whitelist = process.env.WHITELISTED_DOMAINS
+  ? process.env.WHITELISTED_DOMAINS.split(",")
+  : [];
+
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
+
+//Start the server in port 3000
+const port = process.env.PORT ? process.env.PORT : 3000;
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
---
+++ backend/utils/connectdb.ts
@@ -0,0 +1,18 @@
+import mongoose, { ConnectOptions } from "mongoose";
+
+const url = process.env.MONGO_DB_CONNECTION_STRING!!;
+
+const options: ConnectOptions = {
+  user: process.env.MONGO_DB_USER,
+  pass: process.env.MONGO_DB_PASSWORD,
+};
+
+const connect = mongoose.connect(url, options);
+
+connect
+  .then((db) => {
+    console.log("Connected correctly to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
