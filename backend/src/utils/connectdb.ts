70a5ef37c75ea177bb4f01d6d9367c7392db30d3
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2023-07-17 20:39:35 +0300

    Implement ESLint

--- backend/src/utils/connectdb.ts
+++ backend/src/utils/connectdb.ts
@@ -1,16 +1,16 @@
 import mongoose, { ConnectOptions } from "mongoose";
 
-const url = process.env.MONGO_DB_CONNECTION_STRING!!;
+const url = process.env.MONGO_DB_CONNECTION_STRING;
 
 const options: ConnectOptions = {
   user: process.env.MONGO_DB_USER,
   pass: process.env.MONGO_DB_PASSWORD,
 };
 
-const connect = mongoose.connect(url, options);
+const connect = mongoose.connect(url!, options);
 
 connect
-  .then((db) => {
+  .then(() => {
     console.log("Connected correctly to DB");
   })
   .catch((err) => {
